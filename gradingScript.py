6d4cd7214ca7469563b346d8ec6c92012c984e7e
Ricky Barillas <rbarillas3@gatech.edu>
2015-06-29 16:59:30 -0400

    Recursive File Search
    Made a function that navigates the bulk download folder and searches for
    their homework files

--- gradingScript.py
+++ gradingScript.py
@@ -1,8 +1,10 @@
 """
-	1. Navigate through folders and place script inside
+	1. Navigate through folders
 		Use os.listdir() and loop through my students
+		Use recursion?
 	2. Call functions from files in script
 		Use sys.path.append(path)
+	2a. Account for getMediaPath()
 	3. Read output.txt files and ensure they are similar to test case files
 	4. Record how many are right vs wrong
 	5. print score
@@ -11,11 +13,38 @@
 import sys
 import os
 
-path = "C:\Users/rbarillas3\Desktop\RabbitHole".replace("\\", "/")
-sys.path.append(path)
+bulkDownload = "C:\Users/rbarillas3\Downloads\Bulk Download/".replace("\\", "/")
+students = os.listdir(bulkDownload)
 
-from sandbox import test
+def findPYFile(path):
+	#if os.listdir(path) contains a .py file then return true
+	#else dig deeper
 
-print len(sys.path)
+	items = os.listdir(path)
+	if "hw04.py" in items or "HW04.py" in items:
+		return True
+	elif len(items) > 0:
+		return findPYFile(path + "/" + items[0])
+	else:
+		return False
 
-print test()
+#for item in os.listdir(path):
+#	print item
+#	break
+
+#sys.path.append(path)
+
+#from sandbox import test
+
+#print len(sys.path)
+
+#print test()
+
+def navigate():
+	for student in students:
+		path = bulkDownload + student
+		lastName = student.split(",")[0]
+		if not findPYFile(path):
+			print "%s contains the homework file: %s \n" % (lastName, findPYFile(path))
+
+navigate()
